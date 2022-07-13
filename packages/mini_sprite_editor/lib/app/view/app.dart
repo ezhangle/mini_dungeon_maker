b72ba893d4f8a92236dd32a84509c81349a2706b
Erick <erickzanardoo@gmail.com>
2022-07-12 22:04:31 -0300

    feat(mini_sprite_editor): adding project (#30)

--- packages/mini_sprite_editor/lib/app/view/app.dart
+++ packages/mini_sprite_editor/lib/app/view/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:mini_sprite_editor/counter/counter.dart';
 import 'package:mini_sprite_editor/l10n/l10n.dart';
+import 'package:mini_sprite_editor/sprite/sprite.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -9,18 +9,14 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
-        ),
-      ),
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
       localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
       ],
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: const SpritePage(),
     );
   }
 }
--- packages/mini_sprite_editor/lib/main_staging.dart
+++
@@ -1,6 +0,0 @@
-import 'package:mini_sprite_editor/app/app.dart';
-import 'package:mini_sprite_editor/bootstrap.dart';
-
-void main() {
-  bootstrap(() => const App());
-}
